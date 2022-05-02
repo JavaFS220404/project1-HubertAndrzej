9c6452d2cc4b04a6790cbde195e189720a7fdbfb
HubertAndrzej <hubertandrzejsobolewski@gmail.com>
2022-05-02 11:39:41 +0200

    Added Type enum and expanded User and Reimbursement classes

--- src/main/java/com/revature/models/Reimbursement.java
+++ src/main/java/com/revature/models/Reimbursement.java
@@ -1,5 +1,8 @@
 package com.revature.models;
 
+import java.io.File;
+import java.sql.Timestamp;
+
 /**
  * This concrete Reimbursement class can include additional fields that can be used for
  * extended functionality of the ERS application.
@@ -15,7 +18,13 @@ package com.revature.models;
  */
 public class Reimbursement extends AbstractReimbursement {
 
-    public Reimbursement() {
+	private Timestamp submissionDate;
+	private Timestamp resolutionDate;
+	private String description;
+	private File receipt;
+	private Type type;
+	
+	public Reimbursement() {
         super();
     }
 
@@ -26,4 +35,53 @@ public class Reimbursement extends AbstractReimbursement {
     public Reimbursement(int id, Status status, User author, User resolver, double amount) {
         super(id, status, author, resolver, amount);
     }
+    
+    public Reimbursement(int id, Status status, User author, User resolver, double amount, Timestamp submissionDate, Timestamp resolutionDate, String description, File receipt, Type type) {
+        super(id, status, author, resolver, amount);
+        this.submissionDate = submissionDate;
+        this.resolutionDate = resolutionDate;
+        this.description = description;
+        this.receipt = receipt;
+        this.type = type;
+    }
+
+	public Timestamp getSubmissionDate() {
+		return submissionDate;
+	}
+
+	public void setSubmissionDate(Timestamp submissionDate) {
+		this.submissionDate = submissionDate;
+	}
+
+	public Timestamp getResolutionDate() {
+		return resolutionDate;
+	}
+
+	public void setResolutionDate(Timestamp resolutionDate) {
+		this.resolutionDate = resolutionDate;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public File getReceipt() {
+		return receipt;
+	}
+
+	public void setReceipt(File receipt) {
+		this.receipt = receipt;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public void setType(Type type) {
+		this.type = type;
+	}
 }
--- src/main/java/com/revature/models/User.java
+++ src/main/java/com/revature/models/User.java
@@ -15,6 +15,10 @@ package com.revature.models;
  *
  */
 public class User extends AbstractUser {
+	
+	private String firstName;
+	private String lastName;
+	private String email;
 
     public User() {
         super();
@@ -27,4 +31,35 @@ public class User extends AbstractUser {
     public User(int id, String username, String password, Role role) {
         super(id, username, password, role);
     }
+    
+    public User(int id, String username, String password, Role role, String firstName, String lastName, String email) {
+        super(id, username, password, role);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
